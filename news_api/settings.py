51fc1c45b360240702e8df1195196be91c7cba34
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2020-02-13 18:07:41 +1100

    removed JWT setting

--- news_api/settings.py
+++ news_api/settings.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
