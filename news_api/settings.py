65fde742c56dce89b74ea64b44dee3dc87991c3f
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2020-02-13 18:04:37 +1100

    added Procfile

--- news_api/settings.py
+++ news_api/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY', SECRET_KEY_DEFAULT)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
